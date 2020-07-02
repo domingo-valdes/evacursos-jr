023593f572b52fbf17700275461fa7c04eb386d8
domingo-valdes <dvaldes3@uc.cl>
2020-07-02 15:50:56 -0600

    error undefined

--- evacursos-jr/src/assets/js/components/App.jsx
+++ evacursos-jr/src/assets/js/components/App.jsx
@@ -3,47 +3,152 @@ import postService from '../services/posts';
 import { hot } from 'react-hot-loader';
 
 export default function App() {
+
   const [loading, setLoading] = useState(false);
   const [post, setPost] = useState();
   const [user, setUser] = useState('');
+  const [token, setToken] = useState();
   const [password, setPassword] = useState('');
-  const fetchNewPost = useCallback(async () => {
+
+  const [code, setCode] = useState('');
+  const [courseName, setCourseName] = useState('');
+  const [department, setDepartment] = useState('');
+  const [campus, setCampus] = useState('');
+
+  const [courses, setCourses] = useState();
+
+  const fetchAuth = useCallback(async () => {
     setLoading(true);
     console.log("user: " + user);
     console.log("password: " + password);
-    const newPost = await postService.getSearchedCourses(user, password).catch(error => console.error('Error cool:', error));
-    setPost(newPost);
+    const user_token = await postService.getToken(user, password).catch(error => console.error('Error cool:', error));
+    setToken(user_token);
     setLoading(false);
   });
 
+  const fetchCourses = useCallback(async () => {
+    setLoading(true);
+    console.log("code: " + code);
+    console.log("course name: " + courseName);
+    console.log("department: " + department);
+    console.log("campus: " + campus);
+    const coursesList = await postService.getCourses(token, code, courseName, department, campus).catch(error => console.error('Error cool:', error));
+    console.log("courses list: ");
+    console.log(coursesList);
+    setCourses(coursesList);
+    console.log("courses var: ");
+    console.log(courses);
+    setLoading(false);
+
+  });
+
   if (loading) return <p> Loading ...</p>;
-  return (
-    <div>
-      <p>Usuario: </p>
-      <div className="field">
-        <input type="text" name="user" onChange={event => setUser(event.target.value)} value={user} noValidate />
+  if (!token) {
+    return (
+      <div>
+        <p>Usuario: </p>
+        <div className="field">
+          <input type="text" name="user" onChange={event => setUser(event.target.value)} />
+        </div>
+  
+        <p>Contraseña Evacursos Jr: </p>
+        <div className="field">
+          <input type="text" onChange={event => setPassword(event.target.value)} />
+        </div>
+  
+        <button type="button" onClick={fetchAuth}> Ingresar </button>
+        { post && (
+          <>
+            <h3>{post.token}</h3>
+          </>
+        )}
+      </div>
+    );
+  }
+
+  if (token && !courses){
+    return (
+      <div>
+        <p>Código: </p>
+        <div className="field">
+          <input type="text" onChange={event => setCode(event.target.value)}  />
+        </div>
+
+        <p>Nombre del curso</p>
+        <div className="field">
+          <input type="text" onChange={event => setCourseName(event.target.value)} />
+        </div>
+
+        <p>Facultad: </p>
+        <div className="field">
+          <input type="text" onChange={event => setDepartment(event.target.value)}  />
+        </div>
+
+        <p>Campus: </p>
+        <div className="field">
+          <input type="text" onChange={event => setCampus(event.target.value)}  />
+        </div>
+
+        <button type="button" onClick={fetchCourses}> Buscar </button>
+
       </div>
 
-      <p>Contraseña Evacursos Jr: </p>
-      <div className="field">
-        <input type="text" name="password" onChange={event => setPassword(event.target.value)} noValidate />
+    )
+  }
+
+  if (token && courses){
+    return (
+      <div>
+        <p>Código: </p>
+        <div className="field">
+          <input type="text" onChange={event => setCode(event.target.value)}  />
+        </div>
+
+        <p>Nombre del curso</p>
+        <div className="field">
+          <input type="text" onChange={event => setCourseName(event.target.value)} />
+        </div>
+
+        <p>Facultad: </p>
+        <div className="field">
+          <input type="text" onChange={event => setDepartment(event.target.value)}  />
+        </div>
+
+        <p>Campus: </p>
+        <div className="field">
+          <input type="text" onChange={event => setCampus(event.target.value)}  />
+        </div>
+
+        <button type="button" onClick={fetchCourses}> Buscar </button>
+        
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Código</th>
+                <th>Nombre</th>
+                <th>Facultad</th>
+                <th>Campus</th>
+                <th>Descripción</th>
+                <th>Interés</th>
+                <th>Dificultad</th>
+                <th>Tiempo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>{ courses[1].data.attributes.code }</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
       </div>
 
-      <button type="button" onClick={fetchNewPost}> Login and Search!</button>
-      { post && (
-        <>
-          <h3>{post.token}</h3>
-        </>
-      )}
-    </div>
-  );
+    )
+  }
+  
+
 
 }
-
-/*
-function App() {
-  return <div>Hello React World!</div>;
-}
-*/
-
-//export default hot(module)(App);
--- evacursos-jr/src/assets/js/services/posts.js
+++ evacursos-jr/src/assets/js/services/posts.js
@@ -1,8 +1,7 @@
-async function getSearchedCourses(email, password) {
+async function getToken(email, password) {
   
   let url = 'https://evacursos.herokuapp.com/api/auth';
   let data = { email, password };
-  // let data = {email: 'dvaldes3@uc.cl', password: '123456' };
   
   let ans = await fetch(url, {
     method: 'POST',
@@ -17,6 +16,28 @@ async function getSearchedCourses(email, password) {
 
 }
 
+async function getCourses(token, code, name, faculty, campus) {
+  
+  let url = 'https://evacursos.herokuapp.com/api/courses/search';
+  let data = { code, name, faculty, campus };
+  const bearedToken = ('Bearer ' + token.token);
+  console.log(bearedToken);
+  
+  let ans = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json',
+      'Authorization' : bearedToken,
+    },
+  });
+
+  return ans.json();
+
+}
+
 export default {
-  getSearchedCourses,
+  getToken,
+  getCourses,
 }
